6be4bab0a94ff5a5c5eca3db51f60e4e9b1cebca
KhamadGek <khawajahammad53@gmail.com>
2021-03-24 20:40:45 +0500

    adding sanity

---
+++ src/client.js
@@ -0,0 +1,8 @@
+import sanityClient from '@sanity/client'
+ 
+/*Used for linking react and Sanity */ 
+
+export default sanityClient({
+    "projectId": "nvoypzt6",
+    "dataset":   "production"
+})
